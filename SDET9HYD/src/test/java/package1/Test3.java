d76d2ede6f4b960041f1acf067a13f264d43a044
Manju <Manju@DESKTOP-50PIISQ>
2022-10-19 11:33:14 +0530

    sum.

---
+++ SDET9HYD/src/test/java/package1/Test3.java
@@ -0,0 +1,18 @@
+package package1;
+
+public class Test3 {
+
+	public static void main(String[] args) {
+		int[] a = { 1, 2, 2, 4, 5 };
+		int sum = 0;
+
+		for (int i = 0; i < a.length; i++) {
+			sum = sum + a[i];
+
+		}
+
+		System.out.println(sum);
+
+	}
+
+}
